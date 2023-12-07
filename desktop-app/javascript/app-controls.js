60eb7ab6202d21ff65de6a092e67f84f4b8d1184
shipbiulder101 <cubecubedproductionsuk@gmail.com>
2023-12-07 23:02:11 +0000

    bug fix attempts

--- desktop-app/javascript/app-controls.js
+++ desktop-app/javascript/app-controls.js
@@ -4,14 +4,15 @@
   var winState = { maximized: true }
 
   function init() {
+    document.getElementById("minimize-btn")
     document.getElementById("minimize-btn").addEventListener("click", function (e) {
       console.log("minimize pressed");
-      var window = remote.getCurrentWindow();
+      var window = remote.getFocusedWindow();
       window.minimize();
     });
 
     document.getElementById("maximize-btn").addEventListener("click", function (e){
-      var window = remote.getCurrentWindow();
+      var window = remote.getFocusedWindow();
       if(winState.maximized == true){
         winState.maximized = false;
         window.setSize(1000, 800, false);
@@ -25,14 +26,15 @@
 
     document.getElementById("close-btn").addEventListener("click", function (e) {
       console.log("close pressed");
-      var window = remote.getCurrentWindow();
+      var window = remote.getFocusedWindow();
       window.close();
     });
   };
 
-  window.onload = function () {
+  document.onreadystatechange = (event) => {
     console.log("Document loaded");
     if (document.readyState == "complete") {
+      console.log(document.readyState);
       init();
     }
   };
